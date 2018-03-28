5ae80e35af0fe7c03456a3a7dcfe03003c5a68b0
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-28 20:43:42 +0200

    drop unnecessary indexes

--- internal/db/models.go
+++ internal/db/models.go
@@ -4,13 +4,13 @@ import "time"
 
 type PoloniexOrderBook struct {
 	ID uint 			`gorm:"primary_key"`
-	MarketID uint 		`gorm:"index"`
+	MarketID uint
 	Time time.Time		`gorm:"primary_key"`
 }
 
 type PoloniexOrder struct {
 	ID uint 			`gorm:"primary_key"`
-	OrderbookID uint 	`gorm:"index"`
+	OrderbookID uint
 	Rate float64
 	Quantity float64
 	// 0: initial, 1: orderBookModify, 2: orderBookRemove
@@ -27,7 +27,7 @@ type PoloniexMarket struct {
 
 type PoloniexTicker struct {
 	ID  uint 			`gorm:"primary_key"`
-	MarketID uint		`gorm:"index"`
+	MarketID uint
 	Ask float64
 	Bid float64
 	Time time.Time		`gorm:"primary_key"`
