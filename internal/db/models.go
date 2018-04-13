dfbe32ce9015c1af8d92a34126c9c3cd42cd82f2
AbeIgnocious <k.l.kubat@gmail.com>
2018-04-13 09:15:24 +0200

    change ID to bigint fields

--- internal/db/models.go
+++ internal/db/models.go
@@ -3,14 +3,14 @@ package db
 import "time"
 
 type PoloniexOrderBook struct {
-	ID uint 			`gorm:"primary_key"`
-	MarketID uint
+	ID uint64 			`gorm:"primary_key"`
+	MarketID uint64
 	Time time.Time		`gorm:"primary_key"`
 }
 
 type PoloniexOrder struct {
-	ID uint 			`gorm:"primary_key"`
-	OrderbookID uint
+	ID uint64 			`gorm:"primary_key"`
+	OrderbookID uint64
 	Rate float64
 	Quantity float64
 	// 0: initial, 1: orderBookModify, 2: orderBookRemove
@@ -20,14 +20,14 @@ type PoloniexOrder struct {
 }
 
 type PoloniexMarket struct {
-	ID uint 			`gorm:"primary_key"`
-	Ticker string		`gorm:"unique_index:idx_market"`
-	Quote string		`gorm:"unique_index:idx_market"`
+	ID uint64 			`gorm:"primary_key"`
+	Ticker string		`gorm:"unique_index:polo_idx_market"`
+	Quote string		`gorm:"unique_index:polo_idx_market"`
 }
 
 type PoloniexTicker struct {
-	ID  uint 			`gorm:"primary_key"`
-	MarketID uint
+	ID  uint64 			`gorm:"primary_key"`
+	MarketID uint64
 	Ask float64
 	Bid float64
 	Time time.Time		`gorm:"primary_key"`
